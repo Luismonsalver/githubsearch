716646ae7bc57f6b351e5f149850683cbf4cfc4b
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-07-07 02:03:38 -0400

    Funciones para uso de API

--- buscador/script.js
+++ buscador/script.js
@@ -5,6 +5,7 @@ const loginUrl = "https://api.github.com/users/"
 
 let userResults = []
 
+//FUNCION DEBOUNCE PARA RETRASAR LA PETICION
 function debounce(func, delay) {
     let timer;
     return function (...args) {
@@ -15,8 +16,39 @@ function debounce(func, delay) {
     };
   }
 
+//FUNCION searchApi SE EJECUTA CADA VEZ QUE SE INGRESA UN DATO EN LA ENTRADA DE TEXTO
   function handleChange(e) {
     console.log(e.target.value);
+    searchApi(input.value);
   }
 
+  //LA PETICION SE RETRASA 2 SEGUNDOS
 input.addEventListener("input", debounce(handleChange, 2000));
+
+//FUNCION QUE TRAE UNA LISTA DE USUARIOS CON NOMBRES DE USUARIOS PARECIDOS A LOS DEL VALOR DE ENTRADA, LUEGO EJECUTA UNA FUNCION
+//QUE BUSCA EL USUARIO ESPECIFICO
+function searchApi(user) {
+    userResults = [];
+    fetch(url + user + "+in:login")
+      .then(response => response.json())
+      .then(data => {
+        const users = data.items;
+        const userSearch = users.slice(0, 3);
+        const searchPromises = userSearch.map(user => searchLogin(user));
+        return Promise.all(searchPromises);
+      })
+      .then(() => {
+        console.log(userResults);
+      });
+  }
+  
+  //DESPUES DE TRAER LOS USUARIOS ESPECIFICOS (MAX 3) LOS GUARDA EN UN ARRAY
+  async function searchLogin(userLogin){
+  
+    const response = await fetch(loginUrl + userLogin.login);
+    const data = await response.json();
+    console.log(data);
+  
+    userResults.push(data)
+  
+  }

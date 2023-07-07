bfe439b13ee548c977a45d0c13802955995027f0
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-07-07 02:41:40 -0400

    Funcion para mostrar los datos en el HTML

--- buscador/script.js
+++ buscador/script.js
@@ -20,35 +20,53 @@ function debounce(func, delay) {
   function handleChange(e) {
     console.log(e.target.value);
     searchApi(input.value);
+    userList.innerHTML = ""; 
   }
 
-  //LA PETICION SE RETRASA 2 SEGUNDOS
+//LA PETICION SE RETRASA 2 SEGUNDOS
 input.addEventListener("input", debounce(handleChange, 2000));
 
 //FUNCION QUE TRAE UNA LISTA DE USUARIOS CON NOMBRES DE USUARIOS PARECIDOS A LOS DEL VALOR DE ENTRADA, LUEGO EJECUTA UNA FUNCION
 //QUE BUSCA EL USUARIO ESPECIFICO
 function searchApi(user) {
-    userResults = [];
-    fetch(url + user + "+in:login")
-      .then(response => response.json())
-      .then(data => {
-        const users = data.items;
-        const userSearch = users.slice(0, 3);
-        const searchPromises = userSearch.map(user => searchLogin(user));
-        return Promise.all(searchPromises);
-      })
-      .then(() => {
-        console.log(userResults);
-      });
-  }
-  
-  //DESPUES DE TRAER LOS USUARIOS ESPECIFICOS (MAX 3) LOS GUARDA EN UN ARRAY
-  async function searchLogin(userLogin){
-  
-    const response = await fetch(loginUrl + userLogin.login);
-    const data = await response.json();
-    console.log(data);
-  
-    userResults.push(data)
-  
-  }
+  userResults = [];
+  fetch(url + user + "+in:login")
+    .then(response => response.json())
+    .then(data => {
+      const users = data.items;
+      const userSearch = users.slice(0, 3);
+      const searchPromises = userSearch.map(user => searchLogin(user));
+      return Promise.all(searchPromises);
+    })
+    .then(() => {
+      console.log(userResults);
+      renderUsers();
+    });
+}
+
+//DESPUES DE TRAER LOS USUARIOS ESPECIFICOS (MAX 3) LOS GUARDA EN UN ARRAY
+async function searchLogin(userLogin){
+
+  const response = await fetch(loginUrl + userLogin.login);
+  const data = await response.json();
+  console.log(data);
+
+  userResults.push(data)
+
+}
+
+//MUESTRA EN EL HTML LOS DATOS QUE YO REQUIERA DEL ARRAY
+function renderUsers() {
+  userResults.forEach(user => {
+    const userElement = document.createElement("div");
+    userElement.classList.add("usuario");
+    userElement.innerHTML = `
+      <h2>${user.login}</h2>
+      <img src="${user.avatar_url}" alt="Avatar">
+      <p>${user.name ? user.name : "N/A"}</p>
+      <p>${user.company ? user.company : "No tiene una compañía"}</p>
+      <!-- Agrega otras propiedades del usuario que deseas mostrar -->
+    `;
+    userList.appendChild(userElement);
+  });
+}

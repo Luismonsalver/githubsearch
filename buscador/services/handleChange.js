695c187beabc7a8384034714a1c43e0f9fb8e601
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-07-07 23:13:07 -0400

    Eliminar espacios en blanco

--- buscador/services/handleChange.js
+++ buscador/services/handleChange.js
@@ -1,6 +1,11 @@
 //FUNCION searchApi SE EJECUTA CADA VEZ QUE SE INGRESA UN DATO EN LA ENTRADA DE TEXTO
+//SI EL USUARIO COLOCA ESPACIOS EN BLANCO, NO SE REALIZARÁ LA PETICIÓN,
+//ADEMÁS SE ELIMINARAN LOS ESPACIOS EN BLANCO AL INICIO Y AL FINAL DEL TEXTO CON input.value.trim()
 function handleChange(e) {
     console.log(e.target.value);
-    searchApi(input.value);
-    userList.innerHTML = ""; 
+    const userInput = input.value.trim();
+    if (userInput) {
+      searchApi(userInput);
+      userList.innerHTML = "";
+    }
   }
